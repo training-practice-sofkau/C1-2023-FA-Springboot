02bf8385e262cc1cd5254fb7fcc8377dd1ddc6ab
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-14 11:11:06 -0500

    Configure the course entity relationship

--- src/main/java/co/com/sofka/catalog/entity/Course.java
+++ src/main/java/co/com/sofka/catalog/entity/Course.java
@@ -1,8 +1,13 @@
 package co.com.sofka.catalog.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,24 +20,32 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "course")
 public class Course {
+
     @GenericGenerator(name="UUID",
             strategy = "co.com.sofka.catalog.utils.UUIDGeneratorTruncated")
     @GeneratedValue(generator = "UUID")
     @Id
     private String id;
 
+    @Column
     private String name;
 
+    @Column
     private String coach;
 
+    @Column
     private Integer level;
 
+    @Column
     private LocalDate lastUpdated;
 
-    //OneToMany
+    @OneToMany(
+            mappedBy = "course",
+            cascade = CascadeType.ALL,
+            targetEntity = Student.class
+    )
+    @JsonManagedReference
     private List<Student> studentList;
-
-
-
 }
