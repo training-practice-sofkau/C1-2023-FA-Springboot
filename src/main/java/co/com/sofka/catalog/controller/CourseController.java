3343fb614f6a01f8c1b4e27af092481cf674c28d
isan-san <isan.9.9f@gmail.com>
2023-02-14 18:43:23 -0500

    Testing controllers

--- src/main/java/co/com/sofka/catalog/controller/CourseController.java
+++ src/main/java/co/com/sofka/catalog/controller/CourseController.java
@@ -1,4 +1,14 @@
 package co.com.sofka.catalog.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sofkau/courses/")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.OPTIONS, RequestMethod.DELETE}, maxAge = 3600)
 public class CourseController {
+    @GetMapping
+    private ResponseEntity<String> getArtists() {
+        return ResponseEntity.status(200).body("Llego la piton");
+    }
 }
