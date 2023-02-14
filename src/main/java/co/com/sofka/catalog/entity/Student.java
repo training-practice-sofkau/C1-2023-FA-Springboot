27e307ae8237783f056c4b679f446d5469c4b831
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-14 11:10:38 -0500

    Configure the student entity relationship

--- src/main/java/co/com/sofka/catalog/entity/Student.java
+++ src/main/java/co/com/sofka/catalog/entity/Student.java
@@ -1,8 +1,15 @@
 package co.com.sofka.catalog.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ForeignKey;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +19,7 @@ import org.hibernate.annotations.GenericGenerator;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "student")
 public class Student {
     @GenericGenerator(name="UUID",
             strategy = "co.com.sofka.catalog.utils.UUIDGeneratorTruncated")
@@ -19,17 +27,30 @@ public class Student {
     @Id
     private String id;
 
+    @Column
     private String name;
 
+    @Column
     private String idNum;
 
+    @Column
     private Integer age;
 
+    @Column
     private String mail;
 
+    @Column
     private Integer numCourses;
 
-    //ManyToOne
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            targetEntity = Course.class
+    )
+    @JoinColumn(
+            name = "course_id",
+            foreignKey = @ForeignKey(name = "FK_course_id")
+    )
+    @JsonBackReference
     private Course course;
 
 }
