35213825e3f4675615a97034e0450eca0061310b
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-14 16:33:08 -0500

    main services of studen implemented

--- src/main/java/co/com/sofka/catalog/service/IStudentService.java
+++ src/main/java/co/com/sofka/catalog/service/IStudentService.java
@@ -1,15 +1,19 @@
 package co.com.sofka.catalog.service;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.dto.StudentDTO;
+import co.com.sofka.catalog.entity.Course;
+import co.com.sofka.catalog.entity.Student;
 
 import java.util.List;
 
 public interface IStudentService {
+    Student dtoToEntity(StudentDTO studentDTO);
+    StudentDTO entityToDTO(Student student);
     List<StudentDTO> getAllStudents();
     StudentDTO getByIdentificationNumber(String idNum);
-
     StudentDTO getByName(String s);
     StudentDTO saveStudent(StudentDTO studentDTO);
-    StudentDTO editStudent(StudentDTO studentDTO);
-    String deleteStudent(String idNum);
+    StudentDTO editStudent(StudentDTO studentDTO, String studentID);
+    String deleteStudent(String studentID);
 }
--- src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
@@ -1,41 +1,100 @@
 package co.com.sofka.catalog.service.impl;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.dto.StudentDTO;
+import co.com.sofka.catalog.entity.Course;
+import co.com.sofka.catalog.entity.Student;
+import co.com.sofka.catalog.repository.CourseRepository;
+import co.com.sofka.catalog.repository.StudentRepository;
 import co.com.sofka.catalog.service.IStudentService;
+import co.com.sofka.catalog.utils.ExceptionsHandler;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 public class StudentServiceImpl implements IStudentService {
 
+    private StudentRepository studentRepository;
+    private ModelMapper modelMapper;
+
+    public StudentServiceImpl(StudentRepository studentRepository, ModelMapper modelMapper) {
+        this.studentRepository = studentRepository;
+        this.modelMapper = modelMapper;
+    }
 
     @Override
+    public Student dtoToEntity(StudentDTO studentDTO) {
+        return modelMapper.map(studentDTO, Student.class);
+    }
+
+    @Override
+    public StudentDTO entityToDTO(Student student) {
+        return modelMapper.map(student, StudentDTO.class);
+    }
+    @Override
     public List<StudentDTO> getAllStudents() {
-       return null;
+
+        return studentRepository.findAll()
+                .stream()
+                .map(this::entityToDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public StudentDTO getByIdentificationNumber(String idNum) {
-        return null;
+        return studentRepository.findAll()
+                .stream()
+                .map(this::entityToDTO)
+                .filter(studentDTO -> studentDTO.getName().equalsIgnoreCase(idNum))
+                .findFirst()
+                .orElseThrow();
     }
 
     @Override
-    public StudentDTO getByName(String s) {
-        return null;
+    public StudentDTO getByName(String name) {
+        return studentRepository.findAll()
+                .stream()
+                .map(this::entityToDTO)
+                .filter(studentDTO -> studentDTO.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow();
     }
 
     @Override
     public StudentDTO saveStudent(StudentDTO studentDTO) {
-        return null;
+        return entityToDTO(studentRepository.save(dtoToEntity(studentDTO)));
     }
 
     @Override
-    public StudentDTO editStudent(StudentDTO studentDTO) {
-        return null;
+    public StudentDTO editStudent(StudentDTO studentDTO, String studentID) {
+        Optional<Student> response = studentRepository.findById(studentID);
+        if (response.isEmpty()) {
+            throw new ExceptionsHandler("Student not found", HttpStatus.NOT_FOUND);
+        }
+        StudentDTO oldStudentDTO = entityToDTO(response.get());
+        oldStudentDTO.setName(studentDTO.getName());
+        oldStudentDTO.setAge(studentDTO.getAge());
+        oldStudentDTO.setIdNum(studentDTO.getIdNum());
+        oldStudentDTO.setMail(studentDTO.getMail());
+        oldStudentDTO.setNumCourses(studentDTO.getNumCourses());
+
+        return entityToDTO(studentRepository.save(dtoToEntity(oldStudentDTO)));
     }
 
     @Override
-    public String deleteStudent(String idNum) {
-        return null;
+    public String deleteStudent(String studentID) {
+        Optional<Student> response = studentRepository.findById(studentID);
+        if (response.isEmpty()) {
+            throw new ExceptionsHandler("Student not found", HttpStatus.NOT_FOUND);
+        }
+        studentRepository.deleteById(studentID);
+        return ("The student with ID: "+studentID+ " has been deleted.");
+
     }
 
 }
