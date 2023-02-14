deb5096764dab6cdd13ef266b90b47a2c79a288d
Juangmcz <juangui-089@hotmail.com>
2023-02-14 12:28:20 -0500

    The Student entity was set up.

--- src/main/java/co/com/sofka/catalog/entity/Student.java
+++ src/main/java/co/com/sofka/catalog/entity/Student.java
@@ -1,35 +1,36 @@
 package co.com.sofka.catalog.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @GenericGenerator(name="UUID",
             strategy = "co.com.sofka.catalog.utils.UUIDGeneratorTruncated")
     @GeneratedValue(generator = "UUID")
     @Id
-    private String id;
-
+    private String studentID;
+    @Column
     private String name;
-
+    @Column
     private String idNum;
-
+    @Column
     private Integer age;
-
+    @Column
     private String mail;
-
+    @Column
     private Integer numCourses;
 
     //ManyToOne
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Course.class)
+    @JoinColumn(name="course_id", foreignKey = @ForeignKey(name = "FK_course_id"))
     private Course course;
-
 }
