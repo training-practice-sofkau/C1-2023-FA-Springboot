0cde570b415a3262651c154c70338a80800e98f8
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-02-15 12:41:55 -0500

    Adding the logic to the CourseServiceImpl.java to perform the filter methods from the repository and all the CRUD methods

--- src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
@@ -3,22 +3,31 @@ package co.com.sofka.catalog.service.impl;
 
 import co.com.sofka.catalog.controller.ItemNotFoundException;
 import co.com.sofka.catalog.dto.CourseDTO;
+import co.com.sofka.catalog.dto.StudentDTO;
 import co.com.sofka.catalog.entity.Course;
+import co.com.sofka.catalog.entity.Student;
 import co.com.sofka.catalog.repository.CourseRepository;
+import co.com.sofka.catalog.repository.StudentRepository;
 import co.com.sofka.catalog.service.ICourseService;
 import co.com.sofka.catalog.utils.CustomMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Service
 public class CourseServiceImpl implements ICourseService {
 
     @Autowired
     CourseRepository courseRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Override
     public Course dtoToEntity(CourseDTO courseDTO) {
@@ -39,34 +48,38 @@ public class CourseServiceImpl implements ICourseService {
     }
 
     @Override
+    public CourseDTO getById(String idNum) {
+        return entityToDto(courseRepository.findById(idNum).orElseThrow(() -> new ItemNotFoundException(idNum)));
+    }
+
+    @Override
     public List<CourseDTO> getByName(String name) {
-        return courseRepository.findAll()
+        return courseRepository.findByNameContaining(name)
                 .stream()
-                .filter(course -> Objects.equals(course.getName(), name))
                 .map(this::entityToDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<CourseDTO> getByCoach(String c) {
-        return courseRepository.findAll()
+        return courseRepository.findByCoachContaining(c)
                 .stream()
-                .filter(course -> Objects.equals(course.getCoach(), c))
                 .map(this::entityToDto)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<CourseDTO> getByLevel(String level) {
-        return courseRepository.findAll()
+    public List<CourseDTO> getByLevel(Integer level) {
+        return courseRepository.findByLevel(level)
                 .stream()
-                .filter(course -> Objects.equals(course.getCoach(), level))
                 .map(this::entityToDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     public CourseDTO saveCourse(CourseDTO courseDTO) {
+        courseDTO.setStudentListDTO(new ArrayList<StudentDTO>());
+        courseDTO.setLastUpdated(LocalDate.now());
         return entityToDto(courseRepository.save(dtoToEntity(courseDTO)));
     }
 
@@ -78,16 +91,38 @@ public class CourseServiceImpl implements ICourseService {
                     course.setCoach(courseDTO.getCoach());
                     course.setLevel(courseDTO.getLevel());
                     course.setLastUpdated(LocalDate.now());
-                    course.setStudentList(CustomMapper.listStudent(courseDTO.getStudentListDTO()));
                     return courseRepository.save(course);
                 }).orElseThrow(() -> new ItemNotFoundException(id));
         return entityToDto(courseBody);
     }
 
+    public CourseDTO enrollStudentToCourse(String courseId, String studentsID){
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ItemNotFoundException(courseId));
+        Student student = studentRepository.findById(studentsID).orElseThrow(() -> new ItemNotFoundException(studentsID));
+        course.addStudentToCourse(student);
+        student.addNumCourses();
+        studentRepository.save(student);
+        return entityToDto(courseRepository.save(course));
+    }
+
     @Override
     public CourseDTO deleteCourse(String id) {
         var course = courseRepository.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+        var studentList = course.getStudents();
+        for(Student student : studentList){
+            student.removeNumCourses();
+            studentRepository.save(student);
+        }
         courseRepository.delete(course);
         return entityToDto(course);
+    }
+
+    public CourseDTO deleteStudentToCourse(String courseId, String studentsId) {
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ItemNotFoundException(courseId));
+        Student student = studentRepository.findById(studentsId).orElseThrow(() -> new ItemNotFoundException(studentsId));
+        course.removeStudentToCourse(student);
+        student.removeNumCourses();
+        studentRepository.save(student);
+        return entityToDto(courseRepository.save(course));
     }
 }
