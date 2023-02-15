36a738d05f52d39c8fe5ce2b990f7ec4e73cb215
Juangmcz <juangui-089@hotmail.com>
2023-02-15 08:49:24 -0500

    One attribute name was changed.

--- src/main/java/co/com/sofka/catalog/dto/CourseDTO.java
+++ src/main/java/co/com/sofka/catalog/dto/CourseDTO.java
@@ -1,6 +1,5 @@
 package co.com.sofka.catalog.dto;
 
-import co.com.sofka.catalog.entity.Student;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,15 +10,10 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseDTO {
-    private String id;
-
+    private String courseId;
     private String name;
-
     private String coach;
-
     private List<StudentDTO> studentListDTO;
-
     private Integer level;
-
     private LocalDate lastUpdated;
 }
