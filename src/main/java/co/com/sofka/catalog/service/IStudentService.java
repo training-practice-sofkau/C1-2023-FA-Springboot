d22f5fcfda8183bf611860d15a1d91f828ee2df2
andrscc <andrescc143@gmail.com>
2023-02-15 11:20:24 -0500

    The Student service interface was modified to perform the processes stablished. Some method names and output types were changed

--- src/main/java/co/com/sofka/catalog/service/IStudentService.java
+++ src/main/java/co/com/sofka/catalog/service/IStudentService.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface IStudentService {
     List<StudentDTO> getAllStudents();
-    StudentDTO getByIdentificationNumber(String idNum);
+    StudentDTO getByDni(String dni);
 
-    StudentDTO getByName(String s);
+    List<StudentDTO> getByName(String name);
     StudentDTO saveStudent(StudentDTO studentDTO);
     StudentDTO editStudent(StudentDTO studentDTO);
     String deleteStudent(String idNum);
