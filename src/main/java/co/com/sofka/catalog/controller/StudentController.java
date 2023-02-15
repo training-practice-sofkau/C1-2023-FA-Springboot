5022670a88724601594ba8fbcdb95db9369bab73
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-14 21:22:55 -0500

    Basic logic for Student done

--- src/main/java/co/com/sofka/catalog/controller/StudentController.java
+++ src/main/java/co/com/sofka/catalog/controller/StudentController.java
@@ -1,4 +1,16 @@
 package co.com.sofka.catalog.controller;
 
+import co.com.sofka.catalog.service.impl.StudentServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/students")
+@CrossOrigin
 public class StudentController {
+
+    private final StudentServiceImpl studentService;
 }
--- src/main/java/co/com/sofka/catalog/repository/StudentRepository.java
+++ src/main/java/co/com/sofka/catalog/repository/StudentRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+
+    Student findByName (String name);
+
+    Student findByIdNum (String idNum);
 }
