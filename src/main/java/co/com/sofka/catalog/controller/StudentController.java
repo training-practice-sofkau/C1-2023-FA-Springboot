294acccaf3cd379be2e2695ac074d077a927b681
isan-san <isan.9.9f@gmail.com>
2023-02-15 01:02:59 -0500

    Implement all methods for Student in controller

--- src/main/java/co/com/sofka/catalog/controller/StudentController.java
+++ src/main/java/co/com/sofka/catalog/controller/StudentController.java
@@ -1,5 +1,6 @@
 package co.com.sofka.catalog.controller;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.dto.StudentDTO;
 import co.com.sofka.catalog.service.impl.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,32 @@ public class StudentController {
     @GetMapping
     private ResponseEntity<List<StudentDTO>> getArtists() {
         return ResponseEntity.status(200).body(service.getAll());
+    }
+
+
+    @GetMapping("/name/{name}")
+    private ResponseEntity<List<StudentDTO>> getByName(@PathVariable("name") String name) {
+        return ResponseEntity.status(200).body(service.getByName(name));
+    }
+
+    @GetMapping("/idnum/{idnum}")
+    private ResponseEntity<List<StudentDTO>> getByIdNum(@PathVariable("idnum") String idNum) {
+        return ResponseEntity.status(200).body(service.getByIdNum(idNum));
+    }
+
+    @PostMapping
+    private ResponseEntity<StudentDTO> save(@RequestBody StudentDTO student) {
+        return ResponseEntity.status(200).body(service.save(student));
+    }
+
+    @PatchMapping
+    private ResponseEntity<StudentDTO> update(@RequestBody StudentDTO student) {
+        return ResponseEntity.status(200).body(service.update(student));
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Boolean> delete(@PathVariable("id") String id) {
+        return ResponseEntity.status(200).body(service.delete(id));
     }
 
 }
