b0745fb3938b3251c5ef6cc96c02a1b190a41ac6
William Serna <williamsernac@hotmail.com>
2023-02-15 09:45:22 -0500

    Update CourseRepository.java
    Created custom methods in Repository for the filtering

--- src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
+++ src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
@@ -1,9 +1,18 @@
 package co.com.sofka.catalog.repository;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+
+    List<Course> findByNameContaining(String name);
+
+    List<Course> findByCoachContaining(String name);
+
+    List<Course> findByLevel(String level);
 }
