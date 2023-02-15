b5654a4d092f94127cb9ddccd5e583ec3a00d6ef
William Serna <williamsernac@hotmail.com>
2023-02-15 09:52:25 -0500

    Update CourseServiceImpl.java
    Implemented services for Controller to use

--- src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
@@ -2,40 +2,64 @@ package co.com.sofka.catalog.service.impl;
 
 
 import co.com.sofka.catalog.dto.CourseDTO;
+import co.com.sofka.catalog.entity.Course;
+import co.com.sofka.catalog.repository.CourseRepository;
 import co.com.sofka.catalog.service.ICourseService;
+import co.com.sofka.catalog.utils.CustomMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CourseServiceImpl implements ICourseService {
+    private final CourseRepository courseRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
 
     @Override
-    public List<CourseDTO> getAllCourses() {
-        return null;
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
     }
 
     @Override
-    public CourseDTO getByName(String name) {
-        return null;
+    public List<Course> getByName(String name) {
+        return (courseRepository.findByNameContaining(name));
     }
 
     @Override
-    public List<CourseDTO> getByCoach(String c) {
-        return null;
+    public List<Course> getByCoach(String c) {
+        return courseRepository.findByCoachContaining(c);
     }
 
     @Override
-    public List<CourseDTO> getByLevel(String level) {
-        return null;
+    public List<Course> getByLevel(String level) {
+
+        return courseRepository.findByLevel(level);
+    }
+    @Override
+    public CourseDTO saveCourse(CourseDTO courseDTO) {
+
+        return CustomMapper.courseDTO(courseRepository.save(CustomMapper.course(courseDTO)));
     }
 
     @Override
     public CourseDTO editCourse(CourseDTO courseDTO) {
-        return null;
+        return CustomMapper.courseDTO(courseRepository.save(CustomMapper.course(courseDTO)));
     }
 
     @Override
-    public String deleteCourse(CourseDTO courseDTO) {
-        return null;
+    public String deleteCourse(String id) {
+        Optional<Course> d = courseRepository.findById(id);
+        if (d.isEmpty()) return null;
+
+        else{
+            courseRepository.deleteById(id);
+            return "Course deleted";
+        }
+
     }
 }
