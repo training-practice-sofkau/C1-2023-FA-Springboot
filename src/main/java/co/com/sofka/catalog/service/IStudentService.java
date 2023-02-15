cfe64debef36d29884f7f31b5d50737a6601baa3
William Serna <williamsernac@hotmail.com>
2023-02-15 08:45:53 -0500

    Update IStudentService.java
    Adapted IStudentService for use

--- src/main/java/co/com/sofka/catalog/service/IStudentService.java
+++ src/main/java/co/com/sofka/catalog/service/IStudentService.java
@@ -1,14 +1,15 @@
 package co.com.sofka.catalog.service;
 
 import co.com.sofka.catalog.dto.StudentDTO;
+import co.com.sofka.catalog.entity.Student;
 
 import java.util.List;
 
 public interface IStudentService {
-    List<StudentDTO> getAllStudents();
+    List<Student> getAllStudents();
     StudentDTO getByIdentificationNumber(String idNum);
 
-    StudentDTO getByName(String s);
+    List<Student> getByName(String s);
     StudentDTO saveStudent(StudentDTO studentDTO);
     StudentDTO editStudent(StudentDTO studentDTO);
     String deleteStudent(String idNum);
