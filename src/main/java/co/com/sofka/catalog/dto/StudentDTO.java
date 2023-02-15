05396233428774a3307fada7196e775d60bcbefb
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-15 07:56:55 -0500

    Implements the logic that allows to edit a course an the respective endpoint

--- src/main/java/co/com/sofka/catalog/dto/StudentDTO.java
+++ src/main/java/co/com/sofka/catalog/dto/StudentDTO.java
@@ -1,8 +1,6 @@
 package co.com.sofka.catalog.dto;
 
 import co.com.sofka.catalog.entity.Course;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
