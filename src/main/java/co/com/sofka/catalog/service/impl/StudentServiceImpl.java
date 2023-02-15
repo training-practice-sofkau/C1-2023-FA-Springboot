684d7e4c23a550e514a5c51ef4b93bec36f4dcaf
William Serna <williamsernac@hotmail.com>
2023-02-15 08:48:45 -0500

    Update StudentServiceImpl.java
    Created implementation of services for Student entity

--- src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
@@ -1,41 +1,58 @@
 package co.com.sofka.catalog.service.impl;
 
 import co.com.sofka.catalog.dto.StudentDTO;
+import co.com.sofka.catalog.entity.Student;
+import co.com.sofka.catalog.repository.StudentRepository;
 import co.com.sofka.catalog.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import co.com.sofka.catalog.utils.CustomMapper;
+
 import java.util.List;
+import java.util.Optional;
 
-
+@Service
 public class StudentServiceImpl implements IStudentService {
 
-
+    @Autowired
+    StudentRepository studentRepository;
     @Override
-    public List<StudentDTO> getAllStudents() {
-       return null;
+    public List<Student> getAllStudents() {
+       return studentRepository.findAll();
     }
 
     @Override
     public StudentDTO getByIdentificationNumber(String idNum) {
-        return null;
+
+        return CustomMapper.studentDTO(studentRepository.findByIdNum(idNum));
     }
 
     @Override
-    public StudentDTO getByName(String s) {
-        return null;
+    public List<Student> getByName(String s) {
+
+        return studentRepository.findByNameContaining(s);
     }
 
     @Override
     public StudentDTO saveStudent(StudentDTO studentDTO) {
-        return null;
+
+        return CustomMapper.studentDTO(studentRepository.save(CustomMapper.student(studentDTO)));
     }
 
     @Override
     public StudentDTO editStudent(StudentDTO studentDTO) {
-        return null;
+        return CustomMapper.studentDTO(studentRepository.save(CustomMapper.student(studentDTO)));
     }
 
     @Override
     public String deleteStudent(String idNum) {
-        return null;
+        Optional<Student> d = studentRepository.findById(idNum);
+        if(d.isEmpty())return null;
+
+        else {
+            studentRepository.deleteById(idNum);
+            return "Student deleted";
+        }
     }
 
 }
