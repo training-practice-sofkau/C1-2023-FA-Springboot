0e229fe95c472633d589154a3157a5e56abefd51
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-02-15 12:38:37 -0500

    Added methods in order to use JPARepository to do the Query from the server side

--- src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
+++ src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
@@ -1,9 +1,15 @@
 package co.com.sofka.catalog.repository;
 
 import co.com.sofka.catalog.entity.Course;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+    List<Course> findByNameContaining (String name);
+    List<Course> findByCoachContaining (String coach);
+    List<Course> findByLevel (Integer level);
 }
