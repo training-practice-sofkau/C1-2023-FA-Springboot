0d179d81f226fe41faebda59a4ebf0cbb15c06f8
andrscc <andrescc143@gmail.com>
2023-02-15 11:35:43 -0500

    The implementation of all the method from the Student interface were performed following the WS instructions

--- src/main/java/co/com/sofka/catalog/service/IStudentService.java
+++ src/main/java/co/com/sofka/catalog/service/IStudentService.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public interface IStudentService {
     List<StudentDTO> getAllStudents();
-    StudentDTO getByDni(String dni);
+    List<StudentDTO> getByDni(String dni);
 
     List<StudentDTO> getByName(String name);
     StudentDTO saveStudent(StudentDTO studentDTO);
-    StudentDTO editStudent(StudentDTO studentDTO);
+    StudentDTO editStudent(StudentDTO studentDTO, String studentID);
     String deleteStudent(String idNum);
 }
--- src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/StudentServiceImpl.java
@@ -1,41 +1,86 @@
 package co.com.sofka.catalog.service.impl;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.dto.StudentDTO;
+import co.com.sofka.catalog.entity.Course;
+import co.com.sofka.catalog.entity.Student;
+import co.com.sofka.catalog.repository.StudentRepository;
 import co.com.sofka.catalog.service.IStudentService;
+import co.com.sofka.catalog.utils.CustomMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class StudentServiceImpl implements IStudentService {
 
-
+    @Autowired
+    StudentRepository studentRepository;
     @Override
-    public List<StudentDTO> getAllStudents() {
-       return null;
+    public List<StudentDTO> getAllStudents(){
+        return studentRepository.findAll()
+                .stream().map(CustomMapper::studentDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public StudentDTO getByIdentificationNumber(String idNum) {
-        return null;
+    public List<StudentDTO> getByDni(String dni){
+        return studentRepository.findAll()
+                .stream()
+                .filter(student -> student.getDni().equals(dni))
+                .map(CustomMapper::studentDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public StudentDTO getByName(String s) {
-        return null;
+    public List<StudentDTO> getByName(String name) {
+        return studentRepository.findAll()
+                .stream()
+                .filter(student -> student.getName().contains(name))
+                .map(CustomMapper::studentDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public StudentDTO saveStudent(StudentDTO studentDTO) {
-        return null;
+        return CustomMapper.studentDTO(
+                studentRepository.save(CustomMapper.student(studentDTO))
+        );
     }
 
     @Override
-    public StudentDTO editStudent(StudentDTO studentDTO) {
-        return null;
+    public StudentDTO editStudent(StudentDTO studentDTO, String studentID) {
+        StudentDTO student = CustomMapper.studentDTO(
+                studentRepository
+                        .findById(studentID)
+                        .orElse(new Student()));
+
+        if (student.getId() == null) {
+            return student;
+        }
+        else {
+            student.setName(studentDTO.getName());
+            student.setDni(studentDTO.getDni());
+            student.setAge(studentDTO.getAge());
+            student.setEmail(studentDTO.getEmail());
+            student.setCourseDTO(studentDTO.getCourseDTO());
+
+            studentRepository.save(CustomMapper.student(student));
+
+            return student;
+        }
     }
 
     @Override
-    public String deleteStudent(String idNum) {
-        return null;
+    public String deleteStudent(String studentID) {
+        try {
+            studentRepository.deleteById(studentID);
+            return "Deleted";
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return "Unsuccessful";
+        }
     }
-
 }
