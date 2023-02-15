6753f0ca33ca01287b1d81c6bbb5fb6fddf0e128
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-02-15 12:40:53 -0500

    Changing the ICourseService to get the filter methods and all the CRUD methods

--- src/main/java/co/com/sofka/catalog/service/ICourseService.java
+++ src/main/java/co/com/sofka/catalog/service/ICourseService.java
@@ -13,9 +13,11 @@ public interface ICourseService {
     CourseDTO entityToDto (Course course);
 
     List<CourseDTO> getAllCourses();
+    CourseDTO getById(String idNum);
+
     List<CourseDTO> getByName(String name);
     List<CourseDTO> getByCoach(String c);
-    List<CourseDTO> getByLevel(String level);
+    List<CourseDTO> getByLevel(Integer level);
     CourseDTO saveCourse(CourseDTO courseDTO);
     CourseDTO editCourse(String id, CourseDTO courseDTO);
     CourseDTO deleteCourse(String id);
