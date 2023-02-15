4443b4c4785ced3a31b8477ef8b15f458d2129e4
andrscc <andrescc143@gmail.com>
2023-02-15 11:09:21 -0500

    The repository related to the Course table was created

--- src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
+++ src/main/java/co/com/sofka/catalog/repository/CourseRepository.java
@@ -1,8 +1,13 @@
 package co.com.sofka.catalog.repository;
 
+import co.com.sofka.catalog.dto.CourseDTO;
 import co.com.sofka.catalog.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
