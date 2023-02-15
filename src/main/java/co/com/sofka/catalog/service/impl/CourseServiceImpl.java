ecec7b91da5a581f70764de6d7a42e8cbf217ae0
Difsan <difsanchezru@gmail.com>
2023-02-15 00:21:04 -0500

    CourseServiceImpl was completed.

--- src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
+++ src/main/java/co/com/sofka/catalog/service/impl/CourseServiceImpl.java
@@ -7,6 +7,7 @@ import co.com.sofka.catalog.entity.Course;
 import co.com.sofka.catalog.repository.CourseRepository;
 import co.com.sofka.catalog.repository.StudentRepository;
 import co.com.sofka.catalog.service.ICourseService;
+import co.com.sofka.catalog.utils.CustomMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +21,14 @@ public class CourseServiceImpl implements ICourseService {
 
     private StudentRepository studentRepository;
     private CourseRepository courseRepository;
+    private StudentServiceImpl studentService;
 
     public CourseServiceImpl(StudentRepository studentRepository,
-                             CourseRepository courseRepository){
+                             CourseRepository courseRepository,
+                             StudentServiceImpl studentService){
         this.studentRepository = studentRepository;
         this.courseRepository = courseRepository;
+        this.studentService = studentService;
     }
 
 
@@ -97,20 +101,45 @@ public class CourseServiceImpl implements ICourseService {
 
     @Override
     public CourseDTO saveCourse(CourseDTO courseDTO) {
-       /* List<StudentDTO> studentDTOS = courseDTO.getStudentListDTO();
+        List<StudentDTO> studentDTOS = courseDTO.getStudentListDTO();
         if (!studentDTOS.isEmpty()){
             List<Integer> studentDTONotFound = new ArrayList<>();
             studentDTOS.stream().forEach(i->{
-                Optional<StudentDTO> studentDTO = studentRepository
+                Optional<StudentDTO> studentDTO = studentRepository.findById(
+                        i.getIdDTO()
+                ).map(CustomMapper::studentDTO);
+                if (studentDTO.isEmpty()) studentDTONotFound.add(1);
             });
-        }*/
+            if (!studentDTONotFound.isEmpty()) return null;
+        }
         //I need to do the other service first.
-        return null;
+        return courseDTO(courseRepository.save(course(courseDTO)));
     }
 
     @Override
     public CourseDTO editCourse(CourseDTO courseDTO) {
-        return null;
+        Optional<Course> courseUpdate = courseRepository.findById(courseDTO.getIdDTO());
+        List<StudentDTO> studentDTOS = courseDTO.getStudentListDTO();
+        if (courseUpdate.isEmpty()) return null;
+        if (!studentDTOS.isEmpty()){
+            List<Integer> studentDTONotFound = new ArrayList<>();
+            studentDTOS.stream().forEach(i->{
+                Optional<StudentDTO> studentDTO =
+                        studentRepository.findById(i.getIdDTO()).map(CustomMapper::studentDTO);
+                if (studentDTO.isEmpty()) studentDTONotFound.add(1);
+            });
+            if (!studentDTONotFound.isEmpty()) return null;
+        }
+        else {
+            courseUpdate.get().setName(courseDTO.getNameDTO());
+            courseUpdate.get().setCoach(courseDTO.getCoachDTO());
+            courseUpdate.get().setLevel(courseDTO.getLevelDTO());
+            courseUpdate.get().setLastUpdated(courseDTO.getLastUpdatedDTO());
+            courseUpdate.get().setStudentList(courseDTO.getStudentListDTO().stream()
+                    .map(i->this.studentService.student(i))
+                    .collect(Collectors.toList()));
+        }
+        return courseDTO(courseRepository.save(courseUpdate.get()));
     }
 
     @Override
